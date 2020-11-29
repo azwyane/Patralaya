af977b293fb2e4f7cc6cd0f7dd94da0d4f8229ba
azwyane <tunechibaral@gmail.com>
2020-11-29 20:56:49 +0545

    UPDATED:custom tag to get followings of a profile

--- profiles/templatetags/profiles_tags.py
+++ profiles/templatetags/profiles_tags.py
@@ -6,3 +6,7 @@ register = template.Library()
 @register.simple_tag
 def profile_obj(user):
     return Profile.objects.get(user=user)
+
+@register.simple_tag
+def profile_followings(user):
+    return Profile.objects.filter(followers=user).all()

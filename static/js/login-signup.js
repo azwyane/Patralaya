fca393d3d3484ab6123b98062c22e6b9d2997094
sandesh <sandesh0806@gmail.com>
2020-12-05 16:45:31 +0545

    sign in sign up update

---
+++ static/js/login-signup.js
@@ -0,0 +1,28 @@
+
+
+  const inputs = document.querySelectorAll("input");
+
+
+function addcl(){
+	let parent = this.parentNode.parentNode;
+	parent.classList.add("focus");
+  let nextSibiling=parent.nextElementSibling;
+  if(nextSibiling) nextSibiling.style.display="block";
+}
+
+function remcl(){
+	let parent = this.parentNode.parentNode;
+	if(this.value == ""){
+		parent.classList.remove("focus");
+
+	}
+   let nextSibiling=parent.nextElementSibling;
+  if(nextSibiling) nextSibiling.style.display="none";
+}
+
+
+inputs.forEach(input => {
+	input.addEventListener("focus", addcl);
+	input.addEventListener("blur", remcl);
+});
+

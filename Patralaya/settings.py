554425992a913dd823febbcdb126a0cc26fd4ad3
azwyane <tunechibaral@gmail.com>
2020-12-16 19:40:55 +0545

    NEW_FEATURE:Patralaya now supports Nepali language for content display

--- Patralaya/settings.py
+++ Patralaya/settings.py
@@ -70,6 +70,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',  #language select middleware
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -143,7 +144,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ne' #'en-us'
+
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR,'locale')
+]
 
 TIME_ZONE = 'Asia/Kathmandu'
 

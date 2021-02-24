a61853511032ef2252d65dc5f77b289e1ac77340
azwyane <tunechibaral@gmail.com>
2021-02-24 12:37:43 +0545

    WORKS:aggregation

--- apps/feedaggregate/feedrender.py
+++ apps/feedaggregate/feedrender.py
@@ -1,22 +1,10 @@
 import feedparser
 import requests
 
-class Render():
 
-    def __init__(self,*args,**kwargs):
-        self.uri = kwargs['uri']
-
-    def fetch(self):
-        try:
-            response = requests.get(self.uri).text
-        except:
-            return None
-
-        return response   
-
-    def parse(self,context):
-        if context is None:
-            return None
-        else:
-            feed_parsed = feedparser.parse(context)
-            return feed_parsed
+def render(url):
+    response = requests.get(url).text
+    feed_parsed = feedparser.parse(response)
+    if feed_parsed.bozo == 1:
+        return None
+    return feed_parsed
--- apps/feedaggregate/views.py
+++ apps/feedaggregate/views.py
@@ -5,28 +5,19 @@ from feedaggregate import feedrender
 from django.views.generic import TemplateView
 
 class FetchFeedMixin():
-    uri = None
-
-    def get_uri(self):
-        if getattr(self,'uri',None) is None:
-            raise 'No feed url given'
-        return self.uri
 
     def get_rendered_feed(self):
-        feed = feedrender.Render(uri=self.uri)
-        fetch = feed.fetch()
-        feed = feed.parse(fetch)
+        feed = feedrender.render(self.url)
         return feed
 
 class FeedListView(FetchFeedMixin,TemplateView):
     template_name = "feedaggregate/feed_list.html"
-    #temporary uri
-    uri = 'http://127.0.0.1:8000/bundle/published/all/feed.xml'
+    #temporary url
+    url = 'http://arxiv.org/rss/cs?'
 
     def get_context_data(self,**kwargs):
         context = super().get_context_data(**kwargs)
-        feed_uri = self.get_uri()
         feed_rendered = self.get_rendered_feed()
-        context['feed_uri'] = feed_uri
-        context['feed_rendered'] = feed_rendered
+        context['feed_info'] = feed_rendered.feed
+        context['feed_body'] = feed_rendered.entries
         return context
